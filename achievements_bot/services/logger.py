1fa0e0348aaf7840db05d3a923d02e8b294a8e3b
a.groza <a.groza.dev@gmail.com>
2024-08-15 10:14:03 +0300

    Small fixes

--- achievements_bot/services/logger.py
+++ achievements_bot/services/logger.py
@@ -11,7 +11,6 @@ def setup_logging():
 
     log_file = os.path.join(log_dir, 'bot.log')
     debug_file = os.path.join(log_dir, 'debug.log')
-    error_file = os.path.join(log_dir, 'error.log')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     log = logging.getLogger()
@@ -26,12 +25,6 @@ def setup_logging():
     handler = TimedRotatingFileHandler(debug_file, when='midnight', interval=1, backupCount=2, encoding='utf-8')
     handler.suffix = "%d-%m-%Y"
     handler.setLevel(logging.DEBUG)
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    handler = TimedRotatingFileHandler(error_file, when='midnight', interval=1, backupCount=2, encoding='utf-8')
-    handler.suffix = "%d-%m-%Y"
-    handler.setLevel(logging.ERROR)
     handler.setFormatter(formatter)
     log.addHandler(handler)
 
